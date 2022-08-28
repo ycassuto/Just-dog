a034f9687076d5b47d557a2091b05f0a8419e0a6
cassuti <yuvc210@gmail.com>
2022-08-28 17:07:50 +0300

    add server folder, nav bar and pages components

--- just-dog/src/App.js
+++ just-dog/src/App.js
@@ -1,7 +1,11 @@
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
+    <Routes>
+				<Route path="/" element={<HomePage />} />
+			</Routes>
     </>
   );
 }
