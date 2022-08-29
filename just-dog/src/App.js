2f959e5e16881b836fcd02c2e2f1479a6226a631
ycassuto <ycassuto@cyber4s.dev>
2022-08-29 11:43:30 +0300

    edit app

--- just-dog/src/App.js
+++ just-dog/src/App.js
@@ -3,9 +3,7 @@ import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
-    <Routes>
-				<Route path="/" element={<HomePage />} />
-			</Routes>
+    <h1>aaa</h1>
     </>
   );
 }
